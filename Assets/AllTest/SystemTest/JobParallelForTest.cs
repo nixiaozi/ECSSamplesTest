027265f498966a6e4c5a2930fd61b4882f89c0b5
lee cheng <nixiaozi01@hotmail.com>
2020-09-07 00:19:24 +0800

    IJobParallelFor test

--- Assets/AllTest/SystemTest/JobParallelForTest.cs
+++ Assets/AllTest/SystemTest/JobParallelForTest.cs
@@ -6,6 +6,8 @@ using Unity.Mathematics;
 
 public class JobParallelForTest : SystemBase
 {
+
+
     struct ParallelForTestJob : IJobParallelFor
     {
         [DeallocateOnJobCompletion] public NativeArray<ArchetypeChunk> Chunks;
@@ -29,11 +31,31 @@ public class JobParallelForTest : SystemBase
         }
     }
 
+    private EntityQuery m_Query;
 
+    protected override void OnCreate()
+    {
+        m_Query = GetEntityQuery(typeof(B1));
+    }
 
     protected override void OnUpdate()
     {
+
+        var chunks = m_Query.CreateArchetypeChunkArray(Allocator.TempJob);
+
+
         // throw new System.NotImplementedException();
+        var job = new ParallelForTestJob
+        {
+            Chunks = chunks,//EntityManager.GetAllChunks(),
+            DeltaTime = 0.3f,
+            LastSystemVersion = this.LastSystemVersion,
+            TypeB1 = GetArchetypeChunkComponentType<B1>(false),
+            TypeB2 = GetArchetypeChunkComponentType<B2>(true)
+        };
+
+        this.Dependency = job.Schedule(chunks.Length, 32, this.Dependency);
+
     }
 }
 
